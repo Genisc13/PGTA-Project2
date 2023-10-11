51deee5c3b824a3d4c3a40d66d6ab8e19211b579
Alex Ruiz <alex.ruiz.badia@estudiantat.upc.edu>
2023-10-11 23:52:16 +0200

    Añadidas la decodificación de los 3 primeros data items

--- ProyectoPGTA_P2/DataItems.cs
+++ ProyectoPGTA_P2/DataItems.cs
@@ -12,10 +12,21 @@ namespace ProyectoPGTA_P2
     {
         public int number;
         public string[] arrayHex;
+
+        //DATA ITEM 1
+        public string SAC, SIC;
+
         public DataItem1(string[] arrayhex)
         {
             this.number = 1;
-            this.arrayHex = arrayhex;
+            this.arrayHex = arrayhex;  
+            
+            SAC = arrayhex[0];
+            SIC = arrayhex[1];
+
+            Console.WriteLine("SAC: " + SAC);
+            Console.WriteLine("SIC: " + SIC);
+
         }
     }
 
@@ -24,10 +35,33 @@ namespace ProyectoPGTA_P2
     {
         public int number;
         public string[] arrayHex;
+        //public string[] arrayString;
+
+        //DATA ITEM 2
+        public string timestr = "";
+        public int time;
+
         public DataItem2(string[] arrayhex)
         {
             this.number = 2;
             this.arrayHex = arrayhex;
+
+            //this.arrayString = new string[arrayhex.Length];
+            
+            for (int i = 0; i < arrayhex.Length; i++)
+            {
+                //arrayString[i] = Convert.ToString(Convert.ToInt32(arrayhex[i], 16), 2).PadLeft(8, '0');
+                this.timestr += arrayhex[i];
+            }
+            
+            this.time = Convert.ToInt32(timestr)/128; //hora actual en segundos
+            
+            float hours = time / 3600;
+            float minutes = time % 3600 / 60;
+            float seconds = time - hours * 3600 - minutes * 60;
+            Console.WriteLine("Horas: " + hours);
+            Console.WriteLine("Minutos: " + minutes);
+            Console.WriteLine("Segundos: "+ seconds);
         }
     }
 
@@ -36,10 +70,302 @@ namespace ProyectoPGTA_P2
     {
         public int number;
         public string[] arrayHex;
+
+        public string[] arrayString;
+        public string TYP, SIM, RPD, SPI, RAB;
+        public string FX1;
+        public string TST, ERR, XPP, ME, MI, FOE_FRI;
+        public string FX2;
+        public string ADSB_EP, ADSB_VAL, SCN_EP, SCN_VAL, PAI_EP, PAI_VAL;
+
         public DataItem3(string[] arrayhex)
         {
             this.number = 3;
             this.arrayHex = arrayhex;
+            this.arrayString = new string[arrayhex.Length];
+
+            for (int i = 0; i < arrayhex.Length; i++)
+            {
+                arrayString[i] = Convert.ToString(Convert.ToInt32(arrayhex[i], 16), 2).PadLeft(8, '0');
+            }
+
+            //First part decode
+            string a = arrayString[0];
+
+            TYP = String.Concat(a[0], a[1], a[2]);
+            switch (TYP)
+            {
+                case "000":
+                    TYP = "No detection";
+                    break;
+                case "001":
+                    TYP = "Single PSR detection";
+                    break;
+                case "010":
+                    TYP = "Single SSR detection";
+                    break;
+                case "011":
+                    TYP = "SSR + PSR detection";
+                    break;
+                case "100":
+                     TYP = "Single ModeS All-Call";
+                    break;
+                case "101":
+                     TYP = "Single ModeS Roll-Call";
+                    break;
+                case "110":
+                     TYP = "ModeS All-Call + PSR";
+                    break;
+                case "111":
+                    TYP = "ModeS Roll-Call + PSR";
+                    break;
+                default: 
+                    TYP = "N/A";
+                    break;
+            }
+
+            SIM = a[3].ToString();
+            switch (SIM)
+            {
+                case "0":
+                    SIM = "Actual target report";
+                    break;
+                case "1":
+                    SIM = "Simulated target report";
+                    break;
+                default:
+                    SIM = "N/A";
+                    break;
+            }
+
+            RPD = a[4].ToString();
+            switch (RPD) {
+                case "0":
+                    RPD = "Report from RDP Chain 1";
+                    break;
+                case "1":
+                    RPD = "Report from RDP Chain 2";
+                    break;
+                default:
+                    RPD = "N/A";
+                    break;
+            }
+
+            SPI = a[5].ToString();
+            switch (SPI)
+            {
+                case "0":
+                    SPI = "Absence of SPI";
+                    break;
+                case "1":
+                    SPI = "Special Position Identification";
+                    break;
+                default:
+                    SPI = "N/A";
+                    break;
+            }
+
+            RAB = a[6].ToString();
+            switch (RAB)
+            {
+                case "0":
+                    RAB = "Report from aircraft transponder";
+                    break;
+                case "1":
+                    RAB = "Report from field monitor (Fixed transponder)";
+                    break;
+                default:
+                    RAB = "N/A";
+                    break;
+            }
+ 
+            FX1 = a[7].ToString();
+            if (FX1 == "1")
+            {
+                //Decode first extension
+                string b = arrayString[1];
+
+                TST = b[0].ToString();
+                switch (TST)
+                {
+                    case "0":
+                        TST = "Real target report";
+                        break;
+                    case "1":
+                        TST = "Test target report";
+                        break;
+                    default:
+                        TST = "N/A";
+                        break;
+                }
+
+                ERR = b[1].ToString();
+                switch (ERR)
+                {
+                    case "0":
+                        ERR = "No Extended Range";
+                        break;
+                    case "1":
+                        ERR = "Extended Range present";
+                        break;
+                    default:
+                        ERR = "N/A";
+                        break;
+                }
+
+                XPP = b[2].ToString();
+                switch (XPP)
+                {
+                    case "0":
+                        XPP = "No X-Pulse present";
+                        break;
+                    case "1":
+                        XPP = "X-Pulse present";
+                        break;
+                    default:
+                        XPP = "N/A";
+                        break;
+                }
+
+                ME = b[3].ToString();
+                switch (ME)
+                {
+                    case "0":
+                        ME = "No military emergency";
+                        break;
+                    case "1":
+                        ME = "Military emergency";
+                        break;
+                    default:
+                        ME = "N/A";
+                        break;
+                }
+
+                MI = b[4].ToString();
+                switch (MI)
+                {
+                    case "0":
+                        MI = "No military identification";
+                        break;
+                    case "1":
+                        MI = "Military identification";
+                        break;
+                    default:
+                        MI = "N/A";
+                        break;
+                }
+
+                FOE_FRI = String.Concat(b[5], b[6]);
+                switch (FOE_FRI)
+                {
+                    case "00":
+                        FOE_FRI = "No mode 4 interrogation";
+                        break;
+                    case "01":
+                        FOE_FRI = "Friendly target";
+                        break;
+                    case "10":
+                        FOE_FRI = "Unknown target";
+                        break;
+                    case "11":
+                        FOE_FRI = "No reply";
+                        break;
+                    default:
+                        FOE_FRI = "N/A";
+                        break;
+                }
+
+                FX2 = b[7].ToString();
+                if (FX1 == "1")
+                {
+                    //Decode second extension
+                    string c = arrayString[2];
+
+                    ADSB_EP = c[0].ToString();
+                    switch (ADSB_EP)
+                    {
+                        case "0":
+                            ADSB_EP = "On-site ADS-B Information not populated";
+                            break;
+                        case "1":
+                            ADSB_EP = "On-site ADS-B Information populated";
+                            break;
+                        default:
+                            ADSB_EP = "N/A";
+                            break;
+                    }
+
+                    ADSB_VAL = c[1].ToString();
+                    switch (ADSB_VAL)
+                    {
+                        case "0":
+                            ADSB_VAL = "ADSB information not available";
+                            break;
+                        case "1":
+                            ADSB_VAL = "ADSB information available";
+                            break;
+                        default:
+                            ADSB_VAL = "N/A";
+                            break;
+                    }
+
+                    SCN_EP = c[2].ToString();
+                    switch (SCN_EP)
+                    {
+                        case "0":
+                            SCN_EP = "Surveillance Cluster Network Information not populated";
+                            break;
+                        case "1":
+                            SCN_EP = "Surveillance Cluster Network Information populated";
+                            break;
+                        default:
+                            SCN_EP = "N/A";
+                            break;
+                    }
+
+                    SCN_VAL = c[3].ToString();
+                    switch (SCN_VAL)
+                    {
+                        case "0":
+                            SCN_VAL = "SCN information not available";
+                            break;
+                        case "1":
+                            SCN_VAL = "SCN information available";
+                            break;
+                        default:
+                            SCN_VAL = "N/A";
+                            break;
+                    }
+
+                    PAI_EP = c[4].ToString();
+                    switch (PAI_EP)
+                    {
+                        case "0":
+                            PAI_EP = "Passive Element Interface not populated";
+                            break;
+                        case "1":
+                            PAI_EP = "Passive Element Interface populated";
+                            break;
+                        default:
+                            PAI_EP = "N/A";
+                            break;
+                    }
+
+                    PAI_VAL = c[5].ToString();
+                    switch (PAI_VAL)
+                    {
+                        case "0":
+                            PAI_VAL = "PAI information not available";
+                            break;
+                        case "1":
+                            PAI_VAL = "PAI information available";
+                            break;
+                        default:
+                            PAI_VAL = "N/A";
+                            break;
+                    }
+
+                }
         }
     }
 
