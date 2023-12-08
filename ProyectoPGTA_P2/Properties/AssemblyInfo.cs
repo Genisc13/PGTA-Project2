fd7e484c3199aaa2d75ef5fcc0b2db41351d57c4
Alex Ruiz <alex.ruiz.badia@estudiantat.upc.edu>
2023-12-08 01:00:30 +0100

    Cambios finales

--- ProyectoPGTA_P2/Properties/AssemblyInfo.cs
+++ ProyectoPGTA_P2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("ProyectoPGTA_P2")]
+[assembly: AssemblyTitle("ASTERIX DECODER G14 titulo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProyectoPGTA_P2")]
+[assembly: AssemblyCompany("G14")]
+[assembly: AssemblyProduct("ASTERIX DECODER G14 prod")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
